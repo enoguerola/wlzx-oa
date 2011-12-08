d98816256c3ce99b095473dcd77dc9ad9f1b9872
wuyuyue2010 <wuyuyue2010@gmail.com>
2011-12-08 11:30:11 +0000

    配置模型缓存

--- wlzx/src/java/system/wlims/basic/service/teacher/TeacherService.java
+++ wlzx/src/java/system/wlims/basic/service/teacher/TeacherService.java
@@ -2,6 +2,7 @@ package system.wlims.basic.service.teacher;
 
 import java.util.List;
 
+import system.PaginationSupport;
 import system.ServiceException;
 
 import system.wlims.basic.entity.teacher.TeacherModel;
@@ -11,6 +12,7 @@ public interface TeacherService {
 	public TeacherModel save(TeacherModel model)throws ServiceException;
 	
 	public List<TeacherModel> getTeacherModels(TeacherModel model)throws ServiceException;
+	public PaginationSupport<TeacherModel> getTeacherModels(TeacherModel model, int index,int pageSize)throws ServiceException;
 	public void changeStatus(String id, int status)throws ServiceException;
 	
     public void remove(String id)throws ServiceException;
