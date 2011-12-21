b36722e77ef339edd6b62d41fe0a06accb818a5a
wuyuyue2010@gmail.com <wuyuyue2010@gmail.com@406d95a4-6619-280c-e7a0-540618ce770b>
2011-12-21 06:20:41 +0000

    修正接口未定义直接写方法导致的方法找不到错误

--- trunk/wlzx/src/java/system/wlims/oa/service/conference/ConferenceService.java
+++ trunk/wlzx/src/java/system/wlims/oa/service/conference/ConferenceService.java
@@ -2,13 +2,13 @@ package system.wlims.oa.service.conference;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import system.ServiceException;
 import system.wlims.oa.entity.conference.ConferenceModel;
 
 public interface ConferenceService {
 	
-	@SuppressWarnings("unchecked")
 	public void addConference(ConferenceModel Conference, String list);
 	public boolean updateConference(ConferenceModel conference, String adds,String removes);
 	public boolean adjustConferenceById(String id,Date date,String beginTime,String endTime,String placeId);
@@ -21,4 +21,6 @@ public interface ConferenceService {
 			String placeId,String meetingBeginTime, String meetingEndTime,String applyBeginTime, String applyEndTime) ;
 	public List<ConferenceModel> getAttendConferenceByConditions(String userId, String meetingBeginTime, String meetingEndTime) ;
 	public  List<String> validateTimeAndPositionConflict(String id,Date date,String beginTime,String endTime,String placeId);
+	
+	public Map<String, Map<String,Map<String,List<ConferenceModel>>>> getConferencePlaceArrangments(String beginTime,String endTime);
 }
