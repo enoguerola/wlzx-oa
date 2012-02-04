bf129bb75387306bb8be1f5c7fe11e234ea8dd24
wuyuyue2010 <wuyuyue2010@gmail.com>
2012-02-04 01:38:08 +0000

    集成JBPM

---
+++ wlzx/src/java/system/wlims/oa/dao/logistics/PurchaseFormDAO.java
@@ -0,0 +1,10 @@
+package system.wlims.oa.dao.logistics;
+
+
+
+import system.BaseDAO;
+import system.wlims.oa.entity.logistics.PurchaseFormModel;
+
+public interface PurchaseFormDAO  extends BaseDAO<PurchaseFormModel>{
+
+}
